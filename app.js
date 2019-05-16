8497865d32b5a8b716c23ae5a046efc7b6815bce
Sergio <sergiopiermario.placanica@mail.polimi.it>
2019-05-16 14:52:58 +0200

    main.html page added

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const sqlDbFactory = require("knex");
+const process = require("process");
+const path = require('path');
+const _ = require("lodash");
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '/public/pages'));
+let views = path.join(__dirname, '/public/pages');
 module.exports = app;
+
+
+//ROUTES
+
+//Documentations routes
+app.get('/backend/:docpage', function (req, res){
+    res.sendFile(views + '/' + req.params.docpage)
+});
+
+
