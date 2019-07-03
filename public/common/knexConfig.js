2ae07dbf2e4f8d597d0b619072db426d1e3bc8db
Sergio <sergiopiermario.placanica@mail.polimi.it>
2019-07-04 00:07:27 +0200

    added heroku db configuration

--- public/common/knexConfig.js
+++ public/common/knexConfig.js
@@ -6,12 +6,12 @@ module.exports = {
             debug: false,
             client: "pg",
             connection: {
-                user: "postgres",
-                password: "pgadmin95",
-                host: "localhost",
+                user: "grznjjohncvgyj",
+                password: "277680c3b3ed316a28e0fbbe993e96139d997f8392abe868a2371367cc9115b7",
+                host: "ec2-23-21-160-38.compute-1.amazonaws.com",
                 port: 5432,
-                database: "postgres",
-                ssl: false
+                database: "d6nb309co79e8p",
+                ssl: true
             }
         })
     }
